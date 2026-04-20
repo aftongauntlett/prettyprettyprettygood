fb9520d6a3149367d9f61c58fbfd1cbe4e967e3c
Afton Gauntlett <afton.gauntlett@gmail.com>
2026-04-20 19:20:57 -0400

    updating sitemap and image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://prettyprettyprettygood.com",
+  site: "https://www.prettyprettyprettygood.org",
   integrations: [sitemap()],
 });
